30c2c3d939fd7685a416b260a27216b91abbc348
Ameya Bhagat <developameya@outlook.com>
2023-07-30 14:02:23 +0530

    project setup

---
+++ Project.swift
@@ -0,0 +1,61 @@
+import ProjectDescription
+
+//App target constants
+let projName = "Keysmith"
+let orgName = "techRail"
+let projBundleId = "in.techrain.keysmith"
+
+//App unit test target constants
+let projTestsName = projName+"Tests"
+let projTestsBundleId = "in.techrain.keysmithTests"
+
+//App UI test target constants
+let projUITestsName = projName+"UITests"
+let projUITestsBundleId = "in.techrain.keysmithUITests"
+
+let appInfoPlist: [String: InfoPlist.Value] = [
+    "UILaunchScreen": [:]
+]
+
+
+let appTarget = Target(
+    name: projName,
+    platform: .iOS,
+    product: .app,
+    bundleId: projBundleId,
+    infoPlist: .extendingDefault(with: appInfoPlist),
+    sources: ["source/**"]
+)
+
+let unitTestTarget = Target(
+    name: projTestsName,
+    platform: .iOS,
+    product: .unitTests,
+    bundleId: projTestsBundleId,
+    infoPlist: .default,
+    sources: ["\(projName)Tests/**"],
+    dependencies: [
+        .target(name: projName)
+    ]
+)
+
+let uITestTarget = Target(
+    name: projUITestsName,
+    platform: .iOS,
+    product: .uiTests,
+    bundleId: projUITestsBundleId,
+    infoPlist: .default,
+    sources: ["\(projName)UITests/**"],
+    dependencies: [
+        .target(name: projName)
+    ]
+)
+
+let project = Project(
+    name: projName,
+    targets: [
+        appTarget,
+        unitTestTarget,
+        uITestTarget,
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# keysmith-ios
-KeySmith Project for iOS
+#  Keysmith
+
+## Overview
+
+The vision for Keysmith is to let anyone peek at the engine of the software which promises to manage your security while providing the sync features across the board using the solutions that we already use - our favorite cloud solutions. 
+This gives the control back to you. 
+
+## Get started
+
+### Requirements:
+- Xcode 14.3 or above.
+- iOS 16.0 or above.
+
+This project makes use of  [Tuist](https://github.com/tuist/tuist/tree/main). As such this project does not contain Xcode project file (.xcodeproj).
+
+**After cloning the project, please make sure to follow the steps mentioned below, to get this project up and running.**
+
+### 1.  Run the script for "Tuist"
+```shell
+curl -Ls https://install.tuist.io | bash
+```
+### 2.  In terminal, navigate to the root directory of the cloned project. Ensure, it has 'Project.swift' file.
+
+### 3. Run the following command.
+```bash
+tuist generate # Generates Xcode project & workspace
+```
+At this point the project should open in Xcode. If not, open the Xcode workspace file to launch Xcode.
+
