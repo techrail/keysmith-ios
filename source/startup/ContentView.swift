fe5cd16b37b3b4b1e26a69bb2d0ae82300618098
Ameya Bhagat <developameya@outlook.com>
2023-08-29 08:57:00 +0530

    add tab view to the content view.

--- source/startup/ContentView.swift
+++ source/startup/ContentView.swift
@@ -1,8 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTab: Int = 0
+    
     var body: some View {
-        Text("The Keysmith Project")
+        TabView(selection: $selectedTab) {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+                .tag(0)
+            FavouritesView()
+                .tabItem {
+                    Label("Favourites", systemImage: "star")
+                }
+                .tag(1)
+            AddView()
+                .tabItem {
+                    Label("Add", systemImage: "plus")
+                }
+                .tag(2)
+            KeySmithView()
+                .tabItem {
+                    Label("Keysmith", systemImage: "lock.square.stack")
+                }
+                .tag(3)
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+                .tag(4)
+        }
     }
 }
 
