5967ce09bf17df2d9e35b460b9660658b8cb3233
Ameya Bhagat <developameya@outlook.com>
2023-08-31 14:19:59 +0530

    add system image struct to keep track of sf symbols used in the app and have strong type rather than as string.

---
+++ source/lib/core/constants/SystemImage.swift
@@ -0,0 +1,23 @@
+//
+//  SystemImage.swift
+//  Keysmith
+//
+//  Created by Ameya Bhagat on 31/08/23.
+//
+
+import Foundation
+
+///SF symbols used within the application.
+///
+///Prefer to add a property with the name of the symbol required in the app here
+///
+struct SystemImage {
+    static let house: String = "house"
+    static let star: String = "star"
+    static let plus: String = "plus"
+    static let lockSquareStack: String = "lock.square.stack"
+    static let gear: String = "gear"
+    static let keyFill: String = "key.fill"
+    static let noteText: String = "note.text"
+}
+
